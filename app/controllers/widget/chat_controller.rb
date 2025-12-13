1c74c1bef910c3bb51de1a2ee61643166ec7ea0a
Naza-Hash <nazarsun777@gmail.com>
2025-12-13 21:59:22 +0100

    Update chat_controller.rb

--- app/controllers/widget/chat_controller.rb
+++ app/controllers/widget/chat_controller.rb
@@ -1,9 +1,12 @@
-class Widget::ChatController < ApplicationController
-  protect_from_forgery with: :null_session
+module Widget
+  class ChatController < ApplicationController
+    protect_from_forgery with: :null_session
 
-  def reply
-    user_message = params[:message].to_s.strip
-    ai_answer = ChatGptService.new.ask(user_message)
-    render json: { reply: ai_answer }
+    def reply
+      user_message = params[:message].to_s.strip
+      ai_answer = ChatGptService.new.ask(user_message)
+
+      render json: { reply: ai_answer }
+    end
   end
 end
